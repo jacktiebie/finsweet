8f1ac5d50597a38dacc12e38a4175e0ac4465602
Jack <jacktiebie45@gmail.com>
2022-04-18 23:57:16 +0200

    code

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("header");
 
-var btnLeft = document.getElementById('btn_left');
-var btnRight = document.getElementById('btn_right');
-
-
-
-
-btnLeft.addEventListener("click", leftFunction);
-btnRight.addEventListener('click', rightFunction);
-
-function leftFunction() {
-var reviewTwo = document.getElementsByClassName('review-grid-item-3');
-var reviewOne = document.getElementsByClassName('review-grid-item-2'); 
-var displaySetting = reviewOne.style.display;
-
-    if (displaySetting == 'block') {
-        reviewOne.style.display = 'none';
-        reviewTwo.style.display = 'block';
-    } else {
-        reviewOne.style.display = 'block';
-        reviewTwo.style.display = 'none';
-    }
-}
-
-
-
-
-
-
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
